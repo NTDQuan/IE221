e5668d3c0bbba8c873d583fac2088d890441445f
Nguyễn Trần Đình Quân <113564976+NTDQuan@users.noreply.github.com>
2024-06-20 19:18:24 +0700

    Add files via upload

---
+++ Cau_1.3.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Random Words Generator")
+st.subheader("Random Words")
+st.subheader("-")
+st.write("Meaning: -")
+st.write("Click the " + st.button("Generate") + " button to get random words.")
